336a1eb43847f6735d3009184169904102be4b7b
masopego <masopego@gmail.com>
2021-04-08 16:30:29 +0200

    Add styles

--- Practice4/main.js
+++ Practice4/main.js
@@ -8,22 +8,24 @@ const handleLoadFiles = (ev) => {
   const videoType = "video.*";
 
   if (!videoUpload.type.match(videoType)) {
-    videoBox.innerHTML = `<p>
-        El archivo seleccionado no es un vídeo. Por favor, escoge otro archivo
-        en formato vídeo.
+    videoBox.innerHTML = `<p class="videoMessage">
+        El archivo seleccionado no es un vídeo. <span>Por favor, escoge otro archivo
+        en formato vídeo.</span>
       </p>`;
   } else {
     const reader = new FileReader();
     reader.addEventListener("load", (ev) => {
       const result = ev.target.result;
-      videoBox.innerHTML = `<video src=${result} id="video" controls>
+      videoBox.innerHTML = `<video src=${result} id="video" class="video" controls>
           Lo sentimos, pero tu vídeo no ha podido reproducirse
         </video>`;
     });
     reader.addEventListener("progress", (ev) => {
       if (ev.loaded && ev.total) {
         const percent = (ev.loaded / ev.total) * 100;
-        videoBox.innerHTML = `<p class="percentage">${Math.round(percent)}</p>`;
+        videoBox.innerHTML = `<p class="videoMessage">Cargando: <span>${Math.round(
+          percent
+        )}</span></p>`;
       }
     });
     reader.readAsDataURL(videoUpload);
