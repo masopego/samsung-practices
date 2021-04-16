648570c7223967e22a5522651772cb4c279f73c7
masopego <masopego@gmail.com>
2021-04-16 19:59:19 +0200

    Add mongoDB, screenshot and finisihed practice

--- Practice5/main.js
+++ Practice5/main.js
@@ -1,21 +1,47 @@
 const http = require("http");
 const querystring = require("querystring");
+const MongoClient = require("mongodb").MongoClient;
 
 const port = 8000;
+const url = "mongodb://localhost:27017";
 
-const server = http.createServer((request, response) => {
-  const { headers, method, url } = request;
-  console.log("headers: ", headers);
-  console.log("method: ", method);
-  console.log("url: ", url);
+const dbName = "contacts";
+const client = new MongoClient(url, { useUnifiedTopology: true });
+let db;
 
-  if (method === "GET" && url === "/contacts") {
-    response.statusCode = 200;
-    response.setHeader("Content-Type", "text/plain");
-    response.end("Contactos");
+client.connect((err) => {
+  if (err) {
+    console.log("Error al conectar al servidor: ", err);
     return;
   }
-  if (method === "POST" && url === "/contacts") {
+  console.log("Conectado con éxito al servidor");
+  db = client.db(dbName);
+});
+
+const insertDocument = (collectionName, document, callback) => {
+  const collection = db.collection(collectionName);
+  collection.insertOne(document, (err, result) => {
+    if (err) {
+      console.log("Error insertando documento: ", err);
+    }
+    callback(err, result);
+  });
+};
+
+const findDocuments = (collectionName, callback) => {
+  const collection = db.collection(collectionName);
+  collection.find({}).toArray((err, docs) => {
+    if (err) {
+      console.log("Error recuperando documentos: ", err);
+    }
+    callback(err, docs);
+  });
+};
+
+const server = http.createServer((request, response) => {
+  const { method } = request;
+
+  if (method === "POST") {
     let body = [];
     request
       .on("error", (err) => {
@@ -27,10 +53,13 @@ const server = http.createServer((request, response) => {
       .on("end", () => {
         body = Buffer.concat(body).toString();
         const contact = querystring.decode(body);
-        console.log("contact: ", contact);
-        response.statusCode = 200;
-        response.setHeader("Content-Type", "text/plain");
-        response.end("Contactos");
+        insertDocument("contacts", contact, (err, result) => {
+          findDocuments("contacts", (err, docs) => {
+            response.statusCode = 200;
+            response.setHeader("Content-Type", "application/json");
+            response.end(JSON.stringify(docs));
+          });
+        });
       });
 
     return;
